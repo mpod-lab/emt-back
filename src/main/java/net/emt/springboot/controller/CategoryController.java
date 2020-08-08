5d42306d5756e5aa702bf8a38f86672eb67d53f8
mpod-lab <martynapodsiadly@gmail.com>
2020-08-08 11:11:58 +0200

    category controller refactor

--- src/main/java/net/emt/springboot/controller/CategoryController.java
+++ src/main/java/net/emt/springboot/controller/CategoryController.java
@@ -17,55 +17,40 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import net.emt.springboot.exception.ResourceNotFoundException;
 import net.emt.springboot.model.Category;
-import net.emt.springboot.model.Course;
 import net.emt.springboot.repository.CategoryRepository;
+import net.emt.springboot.services.CategoryService;
 
 @RestController
 @RequestMapping("/api/v1/")
 public class CategoryController {
 
 	@Autowired
-	private CategoryRepository categoryRepository;
+	private CategoryService categoryService;
 	
 	@GetMapping("category")
 	public List<Category> getAllCategories() {
-		return this.categoryRepository.findAll();
+		return this.categoryService.getAllCategories();
 	}
 	
 	@GetMapping("/category/{id}")
 	public ResponseEntity<Category> getCategoryById(@PathVariable(value = "id") Long categoryId) throws ResourceNotFoundException {
-		Category category = categoryRepository.findById(categoryId)
-				.orElseThrow(() -> new ResourceNotFoundException("Course not found for this id ::" + categoryId));	
-		return ResponseEntity.ok().body(category);
+		return this.categoryService.getCategoryById(categoryId);
 	}
 	
 	@PostMapping("category")
 	public Category createCategory(@RequestBody Category category) {
-		return this.categoryRepository.save(category);
+		return this.categoryService.saveCategory(category);
 	}
 	
 	
 	@PutMapping("/category/{id}")
 	public ResponseEntity<Category> updateCategory(@PathVariable(value = "id") Long categoryId,
 			@Valid @RequestBody Category categoryDetails) throws ResourceNotFoundException {
-		Category category = categoryRepository.findById(categoryId)
-				.orElseThrow(() -> new ResourceNotFoundException("Course not found for this id :: " + categoryId));
-		
-		category.setCategoryName(categoryDetails.getCategoryName());
-		
-		return ResponseEntity.ok(this.categoryRepository.save(category));
+		return this.categoryService.updateCategory(categoryId, categoryDetails);
 	}
 	
 	@DeleteMapping("/category/{id}")
 	public Map<String, Boolean> deleteCourse(@PathVariable(value = "id") Long categoryId) throws ResourceNotFoundException {
-		Category category = categoryRepository.findById(categoryId)
-				.orElseThrow(() -> new ResourceNotFoundException("Course not found for this id :: " + categoryId));
-		
-		this.categoryRepository.delete(category);
-		
-		Map<String, Boolean> response = new HashMap<>();
-		response.put("deleted", Boolean.TRUE);
-		
-		return response;
+		return this.categoryService.deleteCourse(categoryId);
 	}
 }
---
+++ src/main/java/net/emt/springboot/services/CategoryService.java
@@ -0,0 +1,58 @@
+package net.emt.springboot.services;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import net.emt.springboot.exception.ResourceNotFoundException;
+import net.emt.springboot.model.Category;
+import net.emt.springboot.repository.CategoryRepository;
+
+@Component
+public class CategoryService {
+
+	@Autowired CategoryRepository categoryRepository;
+	
+	public List<Category> getAllCategories() {
+		return this.categoryRepository.findAll();
+	}
+	
+	public ResponseEntity<Category> getCategoryById(Long categoryId) throws ResourceNotFoundException{
+		Category category = categoryRepository.findById(categoryId)
+				.orElseThrow(() -> new ResourceNotFoundException("Course not found for this id ::" + categoryId));	
+		return ResponseEntity.ok().body(category);
+	}
+	
+	public Category saveCategory(Category category) {
+		return this.categoryRepository.save(category);
+	}
+	
+	public ResponseEntity<Category> updateCategory(Long categoryId, Category categoryDetails) throws ResourceNotFoundException {
+		Category category = categoryRepository.findById(categoryId)
+				.orElseThrow(() -> new ResourceNotFoundException("Course not found for this id :: " + categoryId));
+		
+		category.setCategoryName(categoryDetails.getCategoryName());
+		
+		return ResponseEntity.ok(this.categoryRepository.save(category));
+	}
+	
+	public Map<String, Boolean> deleteCourse(Long categoryId) throws ResourceNotFoundException {
+		Category category = categoryRepository.findById(categoryId)
+				.orElseThrow(() -> new ResourceNotFoundException("Course not found for this id :: " + categoryId));
+		
+		this.categoryRepository.delete(category);
+		
+		Map<String, Boolean> response = new HashMap<>();
+		response.put("deleted", Boolean.TRUE);
+		
+		return response;
+	}
+}
